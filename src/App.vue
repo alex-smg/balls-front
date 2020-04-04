69950f16d9be0cb17d004f9cd723fb0dad999209
unknown <alex.s95120@gmail.com>
2020-04-04 17:43:59 +0200

    v2 profile

--- src/App.vue
+++ src/App.vue
@@ -17,25 +17,43 @@
 </script>
 
 <style lang="scss">
-* {
-  padding: 0;
-  margin: 0;
-  outline: none;
-  box-sizing: border-box;
+  * {
+    padding: 0;
+    margin: 0;
+    outline: none;
+    box-sizing: border-box;
 
-}
+  }
 
-h1, h2, h3, h4, h5, p, a, button, input, label {
-  font-family: 'Nunito', sans-serif;
-}
+  h1, h2, h3, h4, h5, p, a, input, label, span {
+    font-family: 'Open Sans', sans-serif;
+  }
+  .hind, button {
+    font-family: 'Hind', sans-serif;
+  }
   .btn-primary {
       background-color: #294FFF;
       border-radius: 2em;
       border: none;
       color: #FFFFFF;
-      padding: 1% 4%;
-      font-weight: 700;
+      padding: 7px 35px;
+      font-weight: 600;
       font-size: 16px;
       width: min-content;
+      white-space: nowrap;
+  }
+  .btn-secondary {
+      background-color: transparent;
+      border-radius: 2em;
+      border: 2px solid #294FFF;
+      color: #294FFF;
+      padding: 7px 35px;
+      font-weight: 600;
+      font-size: 16px;
+      width: min-content;
+      white-space: nowrap;
+  }
+  .flex {
+    display: flex;
   }
 </style>
--- src/components/profil/Profil.vue
+++ src/components/profil/Profil.vue
@@ -1,10 +1,60 @@
 <template>
-    <div>
-        <h2>Profil</h2>
-        <button @click="openModalTeam" class="btn-primary">Créer une équipe</button>
+    <div id="profil">
+        <!--<h2>Profil</h2>
+        <button @click="openModalTeam" class="btn-primary">Créer une équipe</button>-->
+        <div class="containerProfile">
+            <div class="containerLeft">
+                <div class="containerImg">
+                    <div class="maskImg">
+                        <img :src="'http://localhost:3000/'+ currentUser.image">
+                    </div>
+
+                </div>
+                <div>
+                    <button class="btn-primary">Modifier mon profil</button>
+                    <button class="btn-secondary">Créer un tournois</button>
+                    <button class="btn-secondary">Créer une team</button>
+
+                    <button @click="logout">déconnexion</button>
+                </div>
+
+            </div>
+            <div class="containerRight">
+                <section>
+                    <div class="hr"></div>
+                    <div>
+                        <h3 class="hind">Mon profil</h3>
+                        <h2>{{ currentUser.firstname + ' ' + currentUser.lastname }}</h2>
+                        <span>{{ currentUser.gender + ' - ' + birth + ' - ' + age}}</span>
+                        <div class="flex">
+                            <div class="infos">
+                                <p class="min-title hind">Poste</p>
+                                <p class="dataMin-title"> {{ currentUser.postPlayer }}</p>
+                            </div>
+                            <div class="infos">
+                                <p class="min-title hind">Niveau</p>
+                                <p class="dataMin-title">{{ currentUser.level }}</p>
+                            </div>
+                        </div>
+                        <div class="infos">
+                            <p class="min-title hind">Club</p>
+                            <p class="dataMin-title">{{ currentUser.club }}</p>
+                        </div>
+
+                    </div>
+                </section>
+                <section>
+                    <div class="hr"></div>
+                    <h3 class="hind">Mes tournois</h3>
+                </section>
+                <section>
+                    <div class="hr"></div>
+                    <h3 class="hind">Mes teams</h3>
+                </section>
+            </div>
+        </div>
         <modal-team @close-modal="closeModalTeam" v-show="modalTeam"></modal-team>
 
-        <button @click="logout">déconnexion</button>
     </div>
 </template>
 
@@ -16,27 +66,97 @@
         components: {ModalTeam},
         data() {
             return{
-               modalTeam : false,
+                modalTeam : false,
+                currentUser: [],
+                birth: '',
+                age: '',
             }
         },
         methods: {
-            openModalTeam: function () {
+            openModalTeam: function() {
                 this.modalTeam = true
             },
             closeModalTeam: function() {
-                console.log('test')
                this.modalTeam = false
             },
-            logout: function () {
+            logout: function() {
                 store.dispatch('logout')
+            },
+            formatDate: function() {
+                this.currentUser = this.$store.state.userSession;
+                if(this.currentUser.birth) {
+                    let birth = new Date(this.currentUser.birth).toLocaleDateString()
+                    this.birth = birth;
+                    let age = new Date(this.currentUser.birth).getFullYear();
+                    let currentYear = new Date().getFullYear();
+                    this.age = currentYear - age + 'ans'
+                }
             }
         },
         mounted() {
-            console.log(this.$store.state)
+            this.formatDate();
         }
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    #profil {
+        display: flex;
+        .containerProfile {
+            width: 70%;
+            margin: auto;
+            .containerLeft {
+                button {
+                    display: block;
+                    min-width: 100%;
+                    text-overflow: clip;
+                    margin: 2vh 0 2vh 0 ;
+                }
+                .containerImg {
+                    width: 100%;
+                    .maskImg {
+                        width: 10vw;
+                        height: 10vw;
+                        overflow: hidden;
+                        border-radius: 100%;
+                        border: 6px solid #FFCA28;
+                        position: relative;
+                        img {
+                            position: absolute;
+                            width: 110%;
+                        }
+                    }
+                }
+
+            }
+            .containerRight {
+                section {
+                    margin: 4vh 0 4vh 0;
+                    display: flex;
+
+                    .hr {
+                        width: 5px;
+                        background-color: #FFCA28;
+                        margin: 0 20px;
+                        border-radius: 5em;
+                    }
+                    .min-title {
+                        color: #CFCFCF;
+                        font-weight: 600;
+                    }
+                    .dataMin-title {
+                        font-weight: 600;
+                    }
+                    .infos {
+                        margin-right: 40px;
+                    }
+                }
+            }
+            display: flex;
+            h3 {
+                color: #FFCA28;
+            }
+        }
+    }
 
 </style>
