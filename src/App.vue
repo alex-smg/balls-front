31539eee4e0b223a13bd4f20ca24080b78186fc4
unknown <alex.s95120@gmail.com>
2020-04-05 23:33:28 +0200

    add request

--- src/App.vue
+++ src/App.vue
@@ -25,8 +25,9 @@
 
   }
 
-  h1, h2, h3, h4, h5, p, a, input, label, span {
+  h1, h2, h3, h4, h5, p, a, input, label, span, textarea {
     font-family: 'Open Sans', sans-serif;
+    color: #3F3F3F;
   }
   .hind, button {
     font-family: 'Hind', sans-serif;
@@ -36,7 +37,7 @@
       border-radius: 2em;
       border: none;
       color: #FFFFFF;
-      padding: 7px 35px;
+      padding: 5px 35px;
       font-weight: 600;
       font-size: 16px;
       width: min-content;
@@ -47,13 +48,30 @@
       border-radius: 2em;
       border: 2px solid #294FFF;
       color: #294FFF;
-      padding: 7px 35px;
-      font-weight: 600;
+      padding: 5px 35px;
+      font-weigFht: 600;
       font-size: 16px;
       width: min-content;
       white-space: nowrap;
   }
+  .formGroup {
+    input, textarea {
+      width: 100%;
+      box-shadow: 0px 2px 15px #DFDDDD;
+      padding: 10px 20px;
+      border-radius: 0.5em;
+      border: 2px solid transparent;
+      resize: none;
+      transition: 0.5s;
+    }
+    input:focus, textarea:focus {
+      border: 2px solid #294FFF;
+    }
+  }
   .flex {
     display: flex;
+  }
+  button {
+    cursor: pointer;
   }
 </style>
--- src/components/modal/modalteam.vue
+++ src/components/modal/modalteam.vue
@@ -2,27 +2,28 @@
     <div id="modalForm">
         <div class="background" @click.prevent="$emit('close-modal')"></div>
         <div class="containerForm">
-            <form>
+            <div class="header flex hind">
                 <button @click.prevent="$emit('close-modal')">X</button>
+            </div>
+            <form>
+                <h3>Créer une nouvelle team</h3>
                 <div class="formGroup">
-                    <label for="widthTeam">
-                        Nom
-                        <input class="inputText" type="text"  id="name" name="widthTeam">
-                    </label>
+                        <input class="inputText" type="text"  id="name" name="name" v-model="team.name" placeholder="Nom de la team">
                 </div>
                 <div class="formGroup">
-                    <label for="widthTeam">
-                        Description
-                        <textarea class="inputText" type="text"  id="description" name="widthTeam"></textarea>
-                    </label>
+                        <textarea class="inputText" type="text"  id="description" name="description" v-model="team.description" placeholder="Une petite description..."></textarea>
                 </div>
                 <div class="formGroup">
-                    <label for="widthTeam">
-                        Image
-                        <textarea class="inputText" type="file"  id="file" name="widthTeam"></textarea>
-                    </label>
+                        <input class="inputText" type="file"  id="file" name="file">
                 </div>
-                <button>Enregistrer</button>
+                <div class="formGroup">
+                    <div class="flex" v-for="email in team.emailPlayer" :key="email.id">
+                        <input class="inputText" type="text" v-model="team.emailPlayer[email.id].email"  name="player" placeholder="L'email du joueur que tu veux ajouter">
+                        <button v-if="email.id > 0 " @click.prevent="deleteEmailPlayer(email.id)">X</button>
+                    </div>
+                    <button @click.prevent="addEmailPlayer" class="btn-secondary addEmail">+</button>
+                </div>
+                <button class="btn-primary submit"  @click.prevent="createTeam">Enregistrer</button>
             </form>
         </div>
     </div>
@@ -37,13 +38,43 @@
                 team: {
                     name: '',
                     description: '',
-                    name: '',
-                }
+                    emailPlayer: [
+                        {
+                            id: 0,
+                            email: ''
+                        }
+                    ],
+                },
+                currentIdPlayer: 0
             }
         },
         methods: {
-            createTeam: function () {
+            createTeam: async function () {
+                try {
+                    const response = await this.$http.post('http://localhost:3000/team', this.team);
+                    console.log(response)
+                } catch(err) {
+                    console.log(err)
+                }
 
+
+            },
+            addEmailPlayer: function () {
+                console.log(this.currentIdPlayer);
+                this.currentIdPlayer = this.currentIdPlayer + 1;
+
+                let newPlayer = {
+                    id: this.currentIdPlayer,
+                    email: ''
+                };
+                this.team.emailPlayer.push(newPlayer
+                )
+                console.log(this.team)
+            },
+            deleteEmailPlayer: function (id) {
+                console.log(id);
+                this.team.emailPlayer.splice(id, 1);
+                console.log(this.team.emailPlayer)
             }
 
         },
@@ -72,10 +103,56 @@
             width: min-content;
             border-radius: 1em;
             margin: auto;
+            .header {
+                position: relative;
+                button {
+                    position: absolute;
+                    right: 2vh;
+                    top: 1.5vh;
+                    background-color: transparent;
+                    border: none;
+                    font-size: 24px;
+                    font-weight: 800;
+                    color: #FFCA28;
+                    z-index: 1000;
+                }
+            }
             form {
                 padding: 50px;
                 width: 30vw;
                 opacity: 1;
+                position: relative;
+                input, textarea {
+                    margin: 20px 0px;
+                }
+                .addEmail {
+                    width: 40px;
+                    height: 40px;
+                    padding: 0;
+                    border-radius: 100%;
+                    font-size: 24px;
+                    font-weight: 800;
+                    margin:  0px 0px 20px 0px;
+
+                }
+                .submit {
+                    position: absolute;
+                    right: 35px;
+                }
+                .flex {
+                    input {
+                        width: 90%;
+                    }
+                    button {
+                        width: 20%;
+                        background: transparent;
+                        border: none;
+                        font-weight: 600;
+                        color: #294FFF;
+                        font-size: 18px;
+                        cursor: pointer;
+                    }
+                }
             }
         }
     }
